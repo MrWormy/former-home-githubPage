ae4acd70cd3bfa00380a303576a986d22ef01641
Thomas <thomas.laurenc@gmail.com>
2019-06-24 18:48:17 +0200

    SW initialization

---
+++ public/app.js
@@ -0,0 +1,10 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/public/sw.js')
+        .then(function (reg) {
+            // registration worked
+            console.log('SW Registration succeeded. Scope is ' + reg.scope);
+        }).catch(function (error) {
+        // registration failed
+        console.log('SW Registration failed with ' + error);
+    });
+}
---
+++ public/sw.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('applicationCache').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                '/app.js',
+                '/public/loading.png',
+                '/public/icon144.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(caches.match(event.request).then(function(response) {
+        // caches.match() always resolves
+        // but in case of success response will have value
+        if (response !== undefined) {
+            return response;
+        } else {
+            return new Response('Unknown component');
+        }
+    }));
+});
